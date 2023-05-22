98eca0446312c8ff595111af47bab5a78641d920
mariyanixon <134275856+mariyanixon@users.noreply.github.com>
2023-05-22 15:11:38 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,49 @@
+// let fruits=["apple","orange","mango",11];
+// console.log(fruits);
+// let classess=[{
+//     name:"mariya",
+//     age:20,
+//     place:"Tdpa"
+// },
+// {
+//     name:"tiya",
+//     age:10,
+//     place:"koraty"
+
+// }];
+// console.log(classess[1]);
+// function name(){
+//     console.log("Hello")
+// };
+// name()
+// function add(a,b){
+//     console.log(a+b)
+// };
+// add(2,3)
+// function multi(a,b){
+//     console.log(a*b)
+// };
+// multi(2,3)
+// const add1=(a,b)=>{
+//     console.log(a+b)
+// }
+// add1(1,2);
+// let a=20;
+// let b=10;
+// if(a>=b)
+// {
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+// for(let i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+let i=1;
+while(i<=5)
+{
+    console.log(i);
+    ++i;
+}
---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log("Hello World");
+// var a=10;
+// var b=++a;
+
+// console.log(b);
+// console.log(a);
+
+// let a=10;
+// let b="10";
+// if(a===b){
+// console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+let person={
+    name:"tiya",
+    age:10,
+    place:"koraty"
+};
+console.log(`${person.name} lives in ${person.place}`);
+// let fname="mariya";
+// let lname="nixon";
+// let full=`${fname} ${lname}`;
+// console.log(full);
